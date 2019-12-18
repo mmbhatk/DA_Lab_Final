895e4db712b1aa99acf62727c43bc35ae2b01c97
KM Bhat <kmanasvibhat@gmail.com>
2019-12-19 01:03:24 +0530

    Initial commit.

--- 8.R
+++ 8.R
@@ -1,28 +1,46 @@
 library(MASS)
 library(ggplot2)
 
-data = read.csv("8.csv")
+data = read.csv("6.csv")
+
+data$default = gsub("No",0,data$default)
+data$default = gsub("Yes",1,data$default)
+data$student = gsub("No",0,data$student)
+data$student = gsub("Yes",1,data$student)
+
+data$default = as.integer(data$default)
+data$student = as.integer(data$student)
+
+head(data)
 dim(data)
 summary(data)
 
 set.seed(1)
-row.number = sample(1:nrow(data), 0.6*nrow(data))
+row.number = sample(1:nrow(data), 0.6 * nrow(data))
 
 train = data[row.number,]
 test = data[-row.number,]
 dim(train)
 dim(test)
 
-attach(data)
-attach(train)
-attach(test)
-
 model = qda(default ~ student + balance + income, data = train)
 model
 summary(model)
 
-pred1 = predict(model, data = train)
-table(pred1$class, default)
 
-pred2 = predict(model, newdata = test)
-table(pred2$class, default)
+# What should be the output of QDA?
+
+pred.prob_train = predict(model, data = train)
+pred.prob_train = ifelse(unlist(pred.prob_train) > 0.5, 1, 0)
+table(pred.prob_train, data[row.number,]$default)
+
+length(pred.prob_train)
+head(pred.prob_train)
+
+
+pred.prob_test = predict(model, newdata = test)
+table(pred.prob_test, data[-row.number,]$default)
+
+model
+par(mfrow = c(1,1))
+plot(model$x[,1], model$class, col=test$default)
--- practice.r
+++ practice.r
@@ -1,38 +1,26 @@
-library(MASS)
-library(ggplot2)
+stream <- c(1, 2, 3, 5, 9, 2, 3, 8, 10, 16, 19)
+stream <- as.numeric(as.factor(stream))
+print(stream)
 
-data = read.csv("7.csv")
-data$default = gsub("No",0,data$default)
-data$default = gsub("Yes",1,data$default)
-data$student = gsub("No",0,data$student)
-data$student = gsub("Yes",1,data$student)
+# Returns the number of trailing zeroes
+b <- function(x) {
+  num = as.numeric(intToBits(x))
+  index <- match(1, num) - 1
+  return(index)
+}
 
-data$default = as.integer(data$default)
-data$student = as.integer(data$student)
+# Step 1: Calculate the hash values
+H <- c()
+for(i in 1:length(stream)){
+  H[i] <- ((6 * i + 1) %% 5)
+}
 
-head(data)
-dim(data)
-summary(data)
+# Step 2: Count number of trailing zeroes in binary representation
+B <- c()
+for(i in 1:length(H)){
+  B[i] <- b(H[i])
+}
 
-set.seed(1)
-row.number = sample(1:nrow(data), 0.6 * nrow(data))
-
-train = data[row.number,]
-test = data[-row.number,]
-dim(train)
-dim(test)
-
-model = lda(default ~ student + balance + income, data = train)
-model
-summary(model)
-
-pred1 = predict(model, data = train)
-table(pred1$class, data[row.number,]$default)
-
-pred2 = predict(model, newdata = test)
-table(pred2$class, data[-row.number,]$default)
-
-ldahist(pred1$x[,1], g= pred1$class)
-
-par(mfrow=c(1,1))
-plot(pred2$x[,1], pred2$class, col=test$default)
+B[is.na(B)] <- 0
+print(B)
+print(2 ^ max(B))
