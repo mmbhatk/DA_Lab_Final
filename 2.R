7a3d09d088b853580d7883ff482deb7f1553d450
KM Bhat <kmanasvibhat@gmail.com>
2019-12-19 21:26:34 +0530

    Initial commit.

--- 2.R
+++ 2.R
@@ -1,38 +1,23 @@
 data <- read.csv("2.csv", header=TRUE, sep=",")
+
 x1 <- data$experience
 x2 <- data$training
 y <- data$publications
 
-x1_mean <- mean(x1)
-x2_mean <- mean(x2)
-y_mean <- mean(y)
+x0 = rep(1, length(x1))
+X = as.matrix(cbind(x0, x1, x2))
+B = solve(t(X) %*% X) %*% t(X) %*% Y
 
-num1 = sum((x1 - x1_mean) * (y - y_mean))
-dem1 = sum((x1 - x1_mean) ^ 2)
-b1 = num1/dem1
-b1
+b0 = B[1]
+b1 = B[2]
+b2 = B[3]
+data$pred = b0 + b1 * x1 + b2 * x2
 
-num2 = sum((x2 - x2_mean) * (y - y_mean))
-dem2 = sum((x2 - x2_mean) ^ 2)
-b2 = num2/dem2
-b2
-
-b0 = y_mean - b1 * x1_mean - b2 * x2_mean
-b0
-
-data$pred = b0 + b1*x1 + b2*x2
-
-rss = sum((y - data$pred) ^ 2)
-tss = sum((y - y_mean) ^ 2)
-rse = sqrt(rss/(nrow(data) - 2))
-se = 1 -  (rss/tss)
-se
 
 model <- lm(y ~ x1 + x2, data = data)
 summary(model)
 data$pred2 <- predict(model, data)
-data$pred2
 
-plot(data$pred, y)
+
 plot(data$pred, y, xlab = "Predicted", ylab = "Actual")
 abline(a = 0, b = 1)
--- practice.r
+++ practice.r
@@ -1,43 +1,23 @@
-library(digest)
+data <- read.csv("2.csv", header=TRUE, sep=",")
 
-n = 10
-m = 100
-k = 4
-l = rep(0, m)
+x1 <- data$experience
+x2 <- data$training
+y <- data$publications
 
-URL_clicked=c('www.google.com','www.facebook.com','www.yahoo.com','www.gamespot.com','www.ign.com','www.stayfit.com','www.gamer.com','www.gmail.com','www.outlook.com','www.mailer.com')
-URL_notclicked=c('www.insider.com','www.outsider.com','www.paytm.com','www.gpay.com','www.gameology.com','www.youpay.com','www.msrit.com','www.theforum.com','www.bms.com','www.pvr.com')
+x0 = rep(1, length(x1))
+X = as.matrix(cbind(x0, x1, x2))
+B = solve(t(X) %*% X) %*% t(X) %*% Y
+
+b0 = B[1]
+b1 = B[2]
+b2 = B[3]
+data$pred = b0 + b1 * x1 + b2 * x2
 
 
-get_hash = function(item, seed) {
-  hex_str = digest(object = item,
-                   algo = "murmur32",
-                   serialize = F,
-                   seed = seed)
-  hex = paste('0x', hex_str, sep = "")
-  return(as.numeric(hex) %% m)
-}
+model <- lm(y ~ x1 + x2, data = data)
+summary(model)
+data$pred2 <- predict(model, data)
 
-add = function(x) {
-  for(i in 1:k) {
-    hash = get_hash(x, i)
-    l[hash] <<- 1
-  }
-}
 
-check = function(x) {
-  for(i  in 1:k) {
-    hash = get_hash(x, i)
-    if(l[hash] == 0) return(FALSE)
-  }
-  return(TRUE)
-}
-
-for(i in 1:n) add(URL_clicked[i])
-
-test_set = sample(c(URL_clicked, URL_notclicked), n)
-
-for(i in 1:n) {
-  if(check(test_set[i])) cat(test_set[i], "is probably clicked.\n")
-  else cat(test_set[i], "is definitely not clicked.\n")
-}
+plot(data$pred, y, xlab = "Predicted", ylab = "Actual")
+abline(a = 0, b = 1)
